abd19ac3d2f468be2c16af9b1ffed82cb065b023
JuninhoSouza25 <dnt-bh@hotmail.com>
2022-03-29 09:59:54 -0300

    faq page

--- pages/faq.js
+++ pages/faq.js
@@ -1,5 +1,7 @@
-import Link from '../src/components/Link'
-import Head from 'next/head'
+import FAQScreen from '../src/screens/FAQScreen'
+export default FAQScreen;
+
+
 export async function getStaticProps() {
     const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
     const faq = await fetch(FAQ_API_URL)
@@ -11,34 +13,9 @@ export async function getStaticProps() {
             })
     return {
       props: {
-        faq
+            qualquercoisa: 'que eu passar aqui',
+            faq,
       }, // will be passed to the page component as props
     }
   }
 
-export default function FAQPage({ faq }) {
-    console.log(faq)
-
-    return(
-        <div>
-            <Head>
-                <title>FAQ - Alura Cases Campanha</title>
-            </Head>
-            <h1>Alura Cases - FAQ</h1>
-            <Link  href="/">
-                Ir para a Home
-            </Link>
-            <ul>
-                {faq.map(({answer, question}) => (
-                    <li key={question}>
-                        <article>
-                            <h2>{question}</h2>
-                            <p>{answer}</p>
-                        </article>
-                    </li>
-                ))}
-            </ul>
-            
-        </div>
-    )
-}
